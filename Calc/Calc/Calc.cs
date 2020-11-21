275dddc3492d75b88167ebbb552143e1d559abb4
Pavel <luckybuzzz@icloud.com>
2020-11-21 22:50:53 +0300

    Calc

---
+++ Calc/Calc/Calc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Calc
+{
+    class Calc
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число: ");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите действие: ");
+            String act = Console.ReadLine();
+            Console.WriteLine("Введите второе число: ");
+            float y = float.Parse(Console.ReadLine());
+
+            switch (act)
+            {
+                case "+":
+                    float sum = x + y;
+                    Console.WriteLine("Результат: {0}", sum);
+                    break;
+                case "-":
+                    float subt = x - y;
+                    Console.WriteLine("Результат: {0}", subt);
+                    break;
+                case "*":
+                    float mult = x * y;
+                    Console.WriteLine("Результат: {0}", mult);
+                    break;
+                case "/":
+                    float div = x / y;
+                    Console.WriteLine("Результат: {0}", div);
+                    break;
+            }
+        }       
+    }
+}
